98e0c04eb3e37afb10d3ff6114927df90dbdab01
rkhamade <rkhamade4@gmail.com>
2026-02-26 13:34:42 -0500

    Add files via upload
    Initial Code Upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
